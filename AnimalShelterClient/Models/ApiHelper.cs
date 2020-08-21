2c3e747369097e9bc832aabea67def7e61c97b2a
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-08-21 11:42:02 -0700

    add post method to apihelper

--- AnimalShelterClient/Models/ApiHelper.cs
+++ AnimalShelterClient/Models/ApiHelper.cs
@@ -20,5 +20,14 @@ namespace AnimalShelterClient.Models
       var response = await client.ExecuteTaskAsync(request);
       return response.Content;
     }
+
+    public static async Task Post(string newAnimal)
+    {
+      RestClient client = new RestClient("http://localhost:5000/api");
+      RestRequest request = new RestRequest($"animals", Method.POST);
+      request.AddHeader("Content-Type", "application/json");
+      request.AddJsonBody(newAnimal);
+      var response = await client.ExecuteTaskAsync(request);
+    }
   }
 }
