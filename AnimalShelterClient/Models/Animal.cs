80b5f8996aa0c7dc0b0d00c6027b0b5ef518f72f
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-08-21 11:46:23 -0700

    add put method to animal model

--- AnimalShelterClient/Models/Animal.cs
+++ AnimalShelterClient/Models/Animal.cs
@@ -41,13 +41,11 @@ namespace AnimalShelterClient.Models
       var apiCallTask = ApiHelper.Post(jsonAnimal);
     }
 
-    public static async Task Put(int id, string newAnimal)
+    public static void Put(Animal animal)
     {
-      RestClient client = new RestClient("http://localhost:5000/api");
-      RestRequest request = new RestRequest($"animals/{id}", Method.PUT);
-      request.AddHeader("Content-Type", "application/json");
-      request.AddJsonBody(newAnimal);
-      var response = await client.ExecuteTaskAsync(request);
+      string jsonAnimal = JsonConvert.SerializeObject(animal);
+      var apiCallTask = ApiHelper.Put(animal.AnimalId, jsonAnimal);
     }
+    
   }
 }
--- AnimalShelterClient/Models/ApiHelper.cs
+++ AnimalShelterClient/Models/ApiHelper.cs
@@ -29,5 +29,14 @@ namespace AnimalShelterClient.Models
       request.AddJsonBody(newAnimal);
       var response = await client.ExecuteTaskAsync(request);
     }
+
+    public static async Task Put(int id, string newAnimal)
+    {
+      RestClient client = new RestClient("http://localhost:5000/api");
+      RestRequest request = new RestRequest($"animals/{id}", Method.PUT);
+      request.AddHeader("Content-Type", "application/json");
+      request.AddJsonBody(newAnimal);
+      var response = await client.ExecuteTaskAsync(request);
+    }
   }
 }
