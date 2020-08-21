1e9d6e950589fad8a35daf6d1d42d1294c33a702
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-08-21 11:08:40 -0700

    add restsharp and newtonsoft packages

---
+++ AnimalShelterClient/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using RestSharp;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace ApiTest
+{
+  class Program
+  {
+    static void Main()
+    {
+      var apiCallTask = ApiHelper.ApiCall("[YOUR-API-KEY-HERE]");
+      var result = apiCallTask.Result;
+      JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(result);
+      Console.WriteLine(jsonResponse["results"]);
+    }
+  }
+
+  class ApiHelper
+  {
+    public static async Task<string> ApiCall(string apiKey)
+    {
+      RestClient client = new RestClient("https://api.nytimes.com/svc/topstories/v2");
+      RestRequest request = new RestRequest($"home.json?api-key={apiKey}", Method.GET);
+      var response = await client.ExecuteTaskAsync(request);
+      return response.Content;
+    }
+  }
+}
